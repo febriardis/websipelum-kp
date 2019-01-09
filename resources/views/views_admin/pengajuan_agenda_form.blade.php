0a4eccb565e4f6993c084389e021bdf2542a21e1
Febiramdani <1157050054@student.uinsgd.ac.id>
2019-01-10 04:08:26 +0700

    edit-form

--- resources/views/views_admin/pengajuan_agenda_form.blade.php
+++ resources/views/views_admin/pengajuan_agenda_form.blade.php
@@ -19,18 +19,18 @@
 					<label class="col-lg-3 control-label">Nama Agenda</label>
 					<div class="col-lg-9">
 						@if(Auth::user()->ket=='HMJ')
-						<input type="text" class="form-control" name="nm_agenda" readonly="" required= required value="Pemilihan Ketua {{Auth::user()->ket}} {{Auth::user()->ket2}}">
+						<input type="text" class="form-control" name="nm_agenda" readonly="" required= required value="Pemilihan Ketua {{Auth::user()->ket}} {{Auth::user()->ket2}} (Periode {{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')}}-{{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')+1}})">
 						@elseif(Auth::user()->ket=='KPU U')
 						<select class="select" name="nm_agenda" required="" data-placeholder="Pilih Kategori">
 							<option value=""></option>
-							<option>Pemilihan Ketua Sema UIN SGD Bandung</option>
-							<option>Pemilihan Ketua Dema UIN SGD Bandung</option>
+							<option>Pemilihan Ketua Sema UIN SGD Bandung (Periode {{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')}}-{{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')+1}})</option>
+							<option>Pemilihan Ketua Dema UIN SGD Bandung (Periode {{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')}}-{{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')+1}})</option>
 						</select>
 						@else						
 						<select class="select" name="nm_agenda" required="" data-placeholder="Pilih Kategori">
 							<option value=""></option>
-							<option>Pemilihan Ketua Sema Fak. {{Auth::user()->ket2}}</option>
-							<option>Pemilihan Ketua Dema Fak. {{Auth::user()->ket2}}</option>
+							<option>Pemilihan Ketua Sema Fak. {{Auth::user()->ket2}} (Periode {{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')}}-{{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')+1}})</option>
+							<option>Pemilihan Ketua Dema Fak. {{Auth::user()->ket2}} (Periode {{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')}}-{{\Carbon\Carbon::now('Asia/Jakarta')->format('Y')+1}})</option>
 						</select>
 						@endif
 
--- resources/views/views_mahasiswa/form_daftar_calon.blade.php
+++ resources/views/views_mahasiswa/form_daftar_calon.blade.php
@@ -4,21 +4,14 @@
 <!-- Content -->
 <div class="content">	
 	<div style="width: 80%; margin: 20px auto">
-		<div style="margin: 30px 0px;">
-			<h4 style="text-transform: capitalize;">Pendaftaran Bakal Calon Ketua <br> Agenda {{ $tb->nm_agenda }}</h4>
+		<div style="margin: 30px auto; text-align: center;">
+			<h4 style="text-transform: capitalize;">Formulir Pendaftaran Bakal Calon Ketua <br> Agenda {{ $tb->nm_agenda }}</h4>
 			<hr>
 		</div>
 		<form action="/daftar kandidat/{{$tb->id}}" method="POST" enctype="multipart/form-data">
+			<h5 style="text-transform: capitalize;">I. BIODATA DIRI</h5>
 			{{csrf_field()}}
 			<span class="text-danger">{{ $errors->first('foto')}}</span> 
-			<div class="form-group">
-				<div class="row">
-					<label class="control-label col-sm-4">NIM</label>
-					<div class="col-sm-8">
-						<input type="text" class="form-control" required="" name="nim" readonly="readonly" value="{{Auth::user()->nim}}">
-					</div>
-				</div>
-			</div>
 			<div class="form-group">
 				<div class="row">
 					<label class="control-label col-sm-4">Nama</label>
@@ -29,17 +22,38 @@
 			</div>
 			<div class="form-group">
 				<div class="row">
-					<label class="control-label col-sm-4">Jurusan</label>
+					<label class="control-label col-sm-4">Jenis Kelamin</label>
 					<div class="col-sm-8">
-						<input type="text" class="form-control" required="" name="jurusan" readonly="readonly" value="{{Auth::user()->jurusan}}">
+						<label><input type="radio" name="jk" value="lk"> Laki-laki</label><br>
+						<label><input type="radio" name="jk" value="pr"> Perempuan</label>
 					</div>
 				</div>
 			</div>
 			<div class="form-group">
 				<div class="row">
-					<label class="control-label col-sm-4">Tahun Angkatan</label>
+					<label class="control-label col-sm-4">Tempat, Tanggal Lahir</label>
 					<div class="col-sm-8">
-						<input type="text" class="form-control" required="" name="th_angkatan" readonly="readonly" value="{{Auth::user()->th_angkatan}}">
+						<input type="text" class="form-control" required="" name="tmp_lahir" style="width: 30%;float: left;">
+						<input type="date" name="tgl_lahir" class="form-control" style="width: 30%;float: left;">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">NIM</label>
+					<div class="col-sm-8">
+						<input type="text" class="form-control" required="" name="nim" readonly="readonly" value="{{Auth::user()->nim}}">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Jurusan/Fakultas</label>
+					<div class="col-sm-4">
+						<input type="text" class="form-control" required="" name="jurusan" readonly="readonly" value="{{Auth::user()->jurusan}}">
+					</div>
+					<div class="col-sm-4">
+						<input type="text" class="form-control" required="" name="fakultas" readonly="readonly" value="{{Auth::user()->fakultas}}">
 					</div>
 				</div>
 			</div>
@@ -65,16 +79,80 @@
 				        </script>
 				        <!--end-->
 				        <div class="showimage">
-				            <img id="cek-gambar" src="#" alt="image not found" width="100%" height="100%">
+				            <img id="cek-gambar" src="#" alt="" width="100%" height="100%">
 				        </div>
 					</div>
 				</div>
 			</div>
 			<div class="form-group">
 				<div class="row">
-					<label class="control-label col-sm-4">IPK</label>
+					<label class="control-label col-sm-4">Agama</label>
 					<div class="col-sm-8">
-						<input type="text" class="form-control" required="" placeholder="Masukan nilai IPK anda" name="ipk">
+						<input type="text" class="form-control" required="" name="agama">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">No Hp</label>
+					<div class="col-sm-8">
+						<input type="text" class="form-control" required="" name="no_hp">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Email</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Blog/Website</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Anak Ke-, dari - ..saudara</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Asal SMA</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Asal Daerah</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Motto Hidup</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="row">
+					<label class="control-label col-sm-4">Motivasi jadi Ketua HIMATIF</label>
+					<div class="col-sm-8">
+						<input type="email" class="form-control" required="" name="email">
 					</div>
 				</div>
 			</div>
@@ -109,7 +187,7 @@
 					</div>
 				</div>
 			</div>	 -->
-			 <div class="form-group">
+			<div class="form-group">
 				<div class="row">
 					<label class="control-label col-sm-4">Misi</label>
 					<div class="col-sm-8">
@@ -117,13 +195,35 @@
 					</div>
 				</div>
 			</div>
-			
+
+			<hr>
+			<h5 style="text-transform: capitalize;">II. DAFTAR RIWAYAT HIDUP</h5>
+			<div class="form-group">
+				<div class="row">
+					<div class="col-sm-4"></div>
+					<div class="col-sm-8">
+						<input type="file" name="" required=""><br>
+						<a href="" target="_blank">Download Template</a><br>
+					</div>
+				</div>
+			</div>
+			<hr>
 			<div class="form-group form-check">
 				<div class="row">
 					<label class="control-label col-sm-4"></label>
 					<div class="col-sm-8">
 			    		<label class="form-check-label">
 							<input type="checkbox" name="" required=""> Saya menyetujui <a href="javascript::void(0)" data-toggle="modal" data-target="#myModalTerms">kebijakan &amp; persyaratan</a> pencalonan.
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group form-check">
+				<div class="row">
+					<label class="control-label col-sm-4"></label>
+					<div class="col-sm-8">
+			    		<label class="form-check-label">
+							<input type="checkbox" name="" required=""> Saya menyetujui <a href="javascript::void(0)" data-toggle="modal" data-target="#myModalTerms">surat pernyataan</a> pencalonan.
 						</label>
 					</div>
 				</div>
